4cdee045006c8fb5085429e2d7388c7fc093b61d
sasiska-gay <naklsu228@gmail.com>
2021-11-07 22:48:14 +0200

    30.10

---
+++ 30.10/script/script.js
@@ -0,0 +1,38 @@
+//===========Task 1=========//
+
+
+
+// let sum = 0
+// for ( let i = 0 ; i<=100; i += 3) {
+//     if (i >= 40 && i<= 59) {
+//         continue
+//     }
+//     else{
+//         if (i % 2 === 0) {
+//         console.log(`Я цифра ${i} ` )
+//         sum += i
+//     }
+//     }
+    
+//    console.log(sum)
+// }
+
+
+
+//============Task 2=========//
+
+// let age = prompt('Enter your age')
+// if (age === null) {
+//     console.log('Canceled by user')
+// }else if (isNaN(Number(age))) {
+//      console.log('Not an age')
+// }else if (Number(age) <= 2) {
+//      console.log('Немовля')
+// }else if (Number(age) >= 3 && Number(age) <= 18) {
+//    console.log('Дитина')
+// }
+// else if (Number(age) >= 19 && Number(age) <= 150) {
+//     console.log('Дорослий')
+// }else if (Number(age) >= 151) {
+//     console.log('Мав же лежати в могилі')
+// }
