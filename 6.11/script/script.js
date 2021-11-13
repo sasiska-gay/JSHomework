98dcff2167f6e39252abd4d4f733cabbc90ac449
sasiska-gay <naklsu228@gmail.com>
2021-11-13 16:50:10 +0200

    6.11

---
+++ 6.11/script/script.js
@@ -0,0 +1,59 @@
+//==============1 Завдання=========//
+
+
+
+// const mixedArray = [0, 1, false, 2, undefined, '', 3, null];
+
+// const trueArray = [];
+
+// for (let element of mixedArray) {
+//     if (element){
+//         trueArray.push(element)  
+//     }
+    
+// }
+
+// console.log(trueArray)
+
+
+//==============2 Завдання=========//
+
+//==========a========//
+// const rundomNumberArr= [0,5,25,78,95,46,24,65,81,37,154,954];
+
+// let sum = 0;
+// for (let i = 0; i < rundomNumberArr.length; i += 1) {
+//     sum += rundomNumberArr[i]
+// }
+
+// console.log(sum, sum/rundomNumberArr.length)
+
+//==========b========//
+// const rundomNumberArr= [0,5,25,78,95,46,24,65,81,37,154,954];
+// let yourNumber =  27 ; //65 25
+// for (let i = 0; i < rundomNumberArr.length; i += 1) {
+//     if(rundomNumberArr[i] === yourNumber){
+//         console.log('Ми його знайшли')
+//         break
+//     }else {
+//         console.log("Нажаль...")
+//     }
+    
+//}
+
+
+
+
+
+
+//==============3 Завдання=========//
+
+
+// let randomNumbers = []
+
+// for(let i = 0; i < 10; i += 1){
+//     let count = Math.floor(Math.random()*10);
+//     randomNumbers.push(count);
+// }
+
+// console.log(randomNumbers)
