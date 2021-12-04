af2bf8fbb5879e30b804e7bcd295b62b9072ca9f
sasiska-gay <naklsu228@gmail.com>
2021-12-04 10:05:44 +0200

    27/11

---
+++ 27.11/script/script.js
@@ -0,0 +1,92 @@
+//==============1 Task===========//
+
+//  const user = {
+//      name: 'Mango',
+//      age: 20,
+//      hobby: 'html',
+//      premium: true,
+//    }
+// user.mood = 'happy'
+// user.hobby = 'skydiving'
+// user.premium = false
+
+
+// const userKeys = Object.keys(user)
+
+
+
+// for (let key of userKeys){
+//   console.log(`${key}:${user[key]}`)
+// }
+
+//==================2 task================//
+
+
+// const obj =  {
+    
+// }
+
+
+
+// const countProps = function (obj) {
+//     let userValues = Object.values(obj)
+//     console.log(userValues.length)
+//     }
+  
+
+//   /*
+//    * Викличи функції для перевірки працездатності твоєї реалізації.
+//    */
+// countProps({}); // 0
+  
+//  countProps({ name: 'Mango', age: 2 })  ; // 2
+// countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 })
+
+
+
+
+//=============3 task(not working)=====================//
+
+// const findBestEmployee = function (employees) {
+//     let tasks = Object.values(employees)
+//     let names = Object.entries(employees)
+//     let best = Math.max(...tasks)
+//         for(let name of names){
+//             if (names[[1]] = best){
+//                 console.log(names[[0]])
+//             }
+//         }
+        
+//     }
+  
+  
+//   /*
+//    * Викличи функції для перевірки працездатності твоєї реалізації.
+//    */
+//   console.log(
+//     findBestEmployee({
+//       ann: 29,
+//       david: 35,
+//       helen: 1,
+//       lorence: 99,
+//     }),
+//   ); // lorence
+  
+//   console.log(
+//     findBestEmployee({
+//       poly: 12,
+//       mango: 17,
+//       ajax: 4,
+//     }),
+//   ); // mango
+  
+//   console.log(
+//     findBestEmployee({
+//       lux: 147,
+//       david: 21,
+//       kiwi: 19,
+//       chelsy: 38,
+//     }),
+//   ); // lux
+
+
